ff91f1eb5ae291660195ed6949f88baee5988c47
Nahuel Prado <141145234+NaPrado@users.noreply.github.com>
2024-05-17 17:51:20 -0300

    Update Ej15.c
    estaba como el culo

--- PI/Guias/Guia 9/Ej15.c
+++ PI/Guias/Guia 9/Ej15.c
@@ -2,9 +2,9 @@
 
 void
 piramide(int * latas,int * altura){
-    int cantLatas=pow(*(altura+1),2);
-    if (latas<cantLatas){
-        *(altura)++;
+    int cantLatas=pow(*(altura)+1,2);
+    if (*latas>=cantLatas){
+        (*altura)++;
         *latas-=cantLatas;
         piramide(latas,altura);
     }
